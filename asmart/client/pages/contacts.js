51d7780df3fd120fda047c8e976a9a1e8bbd8fdf
talgat55 <lightxdesign55@gmail.com>
2019-11-10 01:29:47 +0600

    add page contacts

---
+++ asmart/client/pages/contacts.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from "../components/layouts/header/header";
+import Footer from "../components/layouts/footer/footer";
+import Hero from "../components/elements/Hero";
+import Title from "../components/elements/Title";
+import Breadcrumb from "../components/elements/Breadcrumbs";
+import FeedbackBlock from "../components/layouts/FeedbackBlock/FeedbackBlock";
+import MapBlock from "../components/layouts/MapBlock/MapBlock";
+
+const Contacts = () => {
+    const title = 'Контакты';
+    let listBread = [{'href': '', 'title': title}];
+    return (
+        <section className="contacts-page">
+            <Header title={title}/>
+            <Hero
+                breadscrumb={<Breadcrumb items={listBread}/>}
+                img="/static/hero-news.jpg"
+            >
+                <Title title={title}  main="true"   className="sub-title single-post"/>
+            </Hero>
+            <MapBlock/>
+            <FeedbackBlock/>
+            <Footer/>
+        </section>
+    )
+};
+
+export default Contacts;
